2b2312e60a5c27fd24e60ad6a1e7fdae4f8bd495
sami-m-g <sami.mg@outlook.com>
2022-10-24 03:44:29 +0200

    Setup v0.1.0 for release

--- pyahn/tile.py
+++ pyahn/tile.py
@@ -1,5 +1,7 @@
 import argparse
+import math
 import os
+import sys
 import urllib.request
 from typing import List
 
@@ -20,56 +22,53 @@ class Tile:
 
     URL_BASE: str = "https://ahn.arcgisonline.nl/arcgis/rest/services/Hoogtebestand/"
     URL_PARAMS: str = "&imageSR=&time=&format=tiff&pixelType=F64&noData=&noDataInterpretation=esriNoDataMatchAny&interpolation=+RSP_BilinearInterpolation&compression=&compressionQuality=&bandIds=&mosaicRule=&renderingRule=&f=image"
-    DATA_RESOLUTION: float = 0.5
     DATA_FACTOR: int = 2
-    DATA_OFFSET: int = 0
+    OUTPUT_DIR = "out"
 
-    def __init__(self, dataset: str, data_dir: str = "out", filename: str = "data.tiff"):
+    def __init__(self, dataset: str, xmin: int, ymax: int, data: np.ndarray):
         self.dataset = dataset
-        self.data_dir = data_dir
-        self.filename = filename
-        
-        self.xmin = 0
-        self.xmax = 0
-        self.ymin = 0
-        self.ymax = 0
-        self.data = None
-
-        self.data_path = os.path.join(data_dir, filename)
-        os.makedirs(data_dir, exist_ok=True)
+        self.xmin = xmin
+        self.ymax = ymax
+        self.data = data
+        # TODO: set resolution according to dataset
+        self.resolution = 0.5
+        self.xmax = xmin + int(data.shape[1] * self.resolution)
+        self.ymin = ymax - int(data.shape[0] * self.resolution)
 
     @classmethod
-    def from_ahn4_array(cls, xy_coords: np.ndarray) -> 'Tile':
+    def from_ahn_array(cls, dataset: str, xy_coords: np.ndarray) -> 'Tile':
         _, xmin, ymin = xy_coords.min(axis=0)
         _, xmax, ymax = xy_coords.max(axis=0)
         print(f"Xmin, Ymin: ({xmin}, {ymin})\tXmax, Ymax: ({xmax}, {ymax})")
-        return cls.from_ahn4_2points(xmin, ymin, xmax, ymax)
+        return cls.from_ahn_2points(dataset, math.floor(xmin), math.floor(ymin), math.ceil(xmax), math.ceil(ymax))
 
     @classmethod
-    def from_ahn4_2points(cls, dataset: str, xmin: int, ymin: int, xmax: int, ymax: int) -> 'Tile':
-        xmin = xmin - 1
-        xmax = xmax + 1
-        ymin = ymin - 1
-        ymax = ymax + 1
+    def from_ahn_2points(cls, dataset: str, xmin: int, ymin: int, xmax: int, ymax: int) -> 'Tile':
+        os.makedirs(Tile.OUTPUT_DIR, exist_ok=True)
+        data_path = os.path.join(Tile.OUTPUT_DIR, f"data_{xmin}_{ymin}-{xmax}_{ymax}.tiff")
 
-        result = Tile(dataset)
         size_x = (xmax - xmin) * cls.DATA_FACTOR
         size_y = (ymax - ymin) * cls.DATA_FACTOR
         urllib.request.urlretrieve(
             f"{cls.URL_BASE}{dataset}/ImageServer/exportImage?bbox={xmin},{ymin},{xmax},{ymax}&bboxSR=&size={size_x},{size_y}{cls.URL_PARAMS}",
-            f"{result.data_path}",
+            data_path,
         ) # note that this might include interpolated values, check the API for the details
-        result.xmin = xmin
-        result.ymax = ymax
-        result.data = imread(result.data_path)
-        result.xmax = xmin + int(result.data.shape[1] * cls.DATA_RESOLUTION)
-        result.ymin = ymax - int(result.data.shape[0] * cls.DATA_RESOLUTION)
-        return result
 
-    def get_z_for_array(self, xy_coords: np.ndarray) -> List[float]:
+        data = imread(data_path)
+        return Tile(dataset, xmin, ymax, data)
+
+    @classmethod
+    def get_z_for_file(cls, dataset: str, input_file: str, output_file: str) -> List[float]:
+        xy_coords = np.loadtxt(input_file, skiprows=1, delimiter=",")
+        tile = Tile.from_ahn_array(dataset, xy_coords)
+
         z_points = []
         for row in range(xy_coords.shape[0]):
-            z_points.append(self.get_z(xy_coords[row, 1], xy_coords[row, 2]))
+            z_points.append(tile.get_z(xy_coords[row, 1], xy_coords[row, 2]))
+
+        xyz_coords = np.insert(xy_coords, 3, z_points, axis=1)
+        np.savetxt(output_file, xyz_coords, delimiter=",", header="ID,X,Y,Z", comments="")
+
         return z_points
 
     def get_z(self, x: float, y: float) -> float:
@@ -81,25 +80,20 @@ class Tile:
             print(f"Invalid point: ({x}, {y}). Y is not within: ({self.ymin}, {self.ymax})")
             return np.nan
 
-        idx = int((x - self.xmin) / self.DATA_RESOLUTION)
-        idy = int((self.ymax - y) / self.DATA_RESOLUTION)
-        return self.data[idy, idx]
+        idx = int((x - self.xmin) / self.resolution)
+        idy = int((self.ymax - y) / self.resolution)
+        return round(self.data[idy, idx], 4)
 
 
-def main():
+def main(args: List[str]) -> List[float]:
     parser = argparse.ArgumentParser(description="Extract Z points for XY coordinates in a csv file.")
     parser.add_argument("-d", "--dataset", help="AHN dataset to use for downloading the tile.", default="AHN4_DTM_50cm", choices=Tile.DATASETS, type=str)
     parser.add_argument("-i", "--input_file", help="Path to the csv file.", required=True, type=str)
     parser.add_argument("-o", "--output_file", help="Path to the csv file.", default="out/xyz.csv", type=str)
-    args = parser.parse_args()
-    
-    xy_coords = np.loadtxt(args.input_file, skiprows=1, delimiter=",")
+    args = parser.parse_args(args)
 
-    tile = Tile.from_ahn4_array(xy_coords)
-    z_coords = tile.get_z_for_array(xy_coords)
-    xyz_coords = np.insert(xy_coords, 3, z_coords, axis=1)
-    np.savetxt(args.output_file, xyz_coords, delimiter=",", header="ID,X,Y,Z", comments="")
+    return Tile.get_z_for_file(args.dataset, args.input_file, args.output_file)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:]) # pragma: no cover
